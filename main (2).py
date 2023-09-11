ca412a713e903703af89b3f6b21838c42ca4dcb9
radhamiru <144670117+radhamiru@users.noreply.github.com>
2023-09-11 14:57:20 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,10 +1,8 @@
-input_year=int(input("enter a year:"))
-def is_leap(year):
-  if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-     return True
-
-     return False
-if is_leap(input_year):
-    print("it is a leap year")
-else:
-    print("it is not a leap year")
+def fact(n):
+  if n == 0:
+    return 1
+  else:
+    return n*fact(n-1)  
+print(fact(0)) 
+print(fact(5)) 
+    
